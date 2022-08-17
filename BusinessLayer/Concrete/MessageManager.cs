c31e1311ab71742945bdb7a567d62076415f034f
shncan <sahincandlbs@gmail.com>
2022-08-17 23:42:20 +0300

    New Attributes and Specs Added

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService  // ilk önce implement ardından messagemanager'dan constructor yapıldı!!
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public Message GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetList()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com"); // bu kullanım doğru değil ancak projenin başlangıcında basit olması için böyle yaptık.
+        }
+
+        public void MessageAdd(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageDelete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
