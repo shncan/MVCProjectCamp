b951ae5f594e44e630103a5a562f4b3765d31bed
shncan <sahincandlbs@gmail.com>
2022-08-21 01:57:48 +0300

    Gallery attributes added, Images Updated. Mvc project fixed.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -19,6 +19,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Heading> Headings { get; set; }
         public DbSet<Writer> Writers { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; }
 
 
         //Yukarıdaki kullanımda örnek olarak Heading bizim burada oluşturduğumuz propertynin adı. Ondan türeyen Headings ise sql'de oluşturulacak tablonun adı.
---
+++ EntityLayer/Concrete/ImageFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class ImageFile   //burada sitemizdeki img'ları tutacağız.
+    {
+        [Key]
+        public int ImageID { get; set; }
+        [StringLength(100)]
+        public string ImageName { get; set; }
+        [StringLength(250)]
+        public string ImagePath { get; set; }
+    }
+}
